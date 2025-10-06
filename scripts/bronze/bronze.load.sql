c65353e87a4bde285a98f220960d6f62aa3d7823
duperdecal <dilshodabdurahmonov10@gmail.com>
2025-10-06 21:30:43 +0500

    Updated comments in bronze.load.sql

--- scripts/bronze/bronze.load.sql
+++ scripts/bronze/bronze.load.sql
@@ -1,8 +1,8 @@
-create or alter procedure bronze.load_bronze as
+create or alter procedure bronze.load_bronze as -- Создание процедуры
 begin
 	declare @start_time datetime, @end_time datetime, @start_time1 datetime, @end_time1 datetime
 begin try
-set @start_time1= GETDATE()
+set @start_time1= GETDATE() -- Создание переменных чтобы выводить в принтах
 		print '========================================'
 		print 'Loading Bronze Layer'
 		print '========================================'
@@ -10,12 +10,12 @@ set @start_time1= GETDATE()
 		print '----------------------------------------'
 		print 'Loading CRM table'
 		print '----------------------------------------'
-		--Loading crm_cust_info into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 		set @start_time = GETDATE();
 		Print '>>>Truncation table crm_cust_info'
 		truncate table bronze.crm_cust_info;
 		Print '>>>Bulk inserting to table crm_cust_info'
-		bulk insert bronze.crm_cust_info
+		bulk insert bronze.crm_cust_info --Загрузка csv файла на таблицу с директории моего компа
 		from 'C:\Users\dilsh\OneDrive\Рабочий стол\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		with (
 		  firstrow= 2,
@@ -25,7 +25,7 @@ set @start_time1= GETDATE()
 		set @end_time = GETDATE();
 		print '>>> Loading duration: '+ cast(datediff(second, @start_time, @end_time) as nvarchar)+ 'seconds'
 
-		--Loading prd_info into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 
 		set @start_time = GETDATE();
 		Print '>>>Truncation table crm_prd_info'
@@ -41,7 +41,7 @@ set @start_time1= GETDATE()
 		set @end_time = getdate()
 		print '>>> Loading duration: '+ cast(datediff(second, @start_time, @end_time) as nvarchar)+ 'seconds'	
 
-		--Loading sales_details into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 		set @start_time = GETDATE();
 		Print '>>>Truncation table crm_sales_details'
 		truncate table bronze.crm_sales_details;
@@ -60,7 +60,7 @@ set @start_time1= GETDATE()
 		print 'Loading ERP tables'
 		print '----------------------------------------'
 
-		--Loading CUST_AZ12 into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 
 		truncate table bronze.erp_CUST_AZ12;
 
@@ -74,7 +74,7 @@ set @start_time1= GETDATE()
 
 	
 
-		--Loading LOC_A101 into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 
 		truncate table bronze.erp_LOC_A101;
 
@@ -89,11 +89,11 @@ set @start_time1= GETDATE()
 	
 
 
-		--Loading PX_CAT_G1V2 into table truncating it in advance
+		--Очистка таблицы перед загрузкой в таблицу
 
 		truncate table bronze.erp_PX_CAT_G1V2;
 
-		bulk insert bronze.erp_PX_CAT_G1V2
+		bulk insert bronze.erp_PX_CAT_G1V2 --Загрузка csv файла на таблицу с директории моего компа
 		from 'C:\Users\dilsh\OneDrive\Рабочий стол\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		with (
 		  firstrow= 2,
@@ -108,6 +108,6 @@ set @start_time1= GETDATE()
 		Print 'Error occured during loading bronze layer'
 		print 'Error message' + error_message();
 		Print '==================================='
-	end catch
+	end catch --Это сообщение выйдет если произойдет ошибка
 	
 
